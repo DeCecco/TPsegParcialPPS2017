368e5551c4452d156a39e0127fd394bdd1101bd7
Pablito De Cecco <pablomdececcorios@gmail.com>
2018-10-16 15:12:41 -0300

    otra app

--- src/pages/estadisticas/estadisticas.ts
+++ src/pages/estadisticas/estadisticas.ts
@@ -68,21 +68,21 @@ export class EstadisticasPage {
     this.returnToken();
     this.setDefault();
   }
-  setDefault(){
+  setDefault() {
     let d = new Date();
     let m = d.getMonth();
     let h = d.getHours()
-    if(m>=6){
-      this.cuatrimestre="2";
-    }else{
-      this.cuatrimestre="1";
+    if (m >= 6) {
+      this.cuatrimestre = "2";
+    } else {
+      this.cuatrimestre = "1";
     }
-    if(h>=18){
-      this.turno='3';
-    }else if(h<=13){
-      this.turno='1';
-    }else{
-      this.turno='2';
+    if (h >= 18) {
+      this.turno = '3';
+    } else if (h <= 13) {
+      this.turno = '1';
+    } else {
+      this.turno = '2';
     }
   }
 
@@ -105,12 +105,12 @@ export class EstadisticasPage {
     console.info(id)
   }
   comboMaterias() {
-    
+
     this.vueltas = 0;
     if (this.anio != 0 && this.cuatrimestre != 0 && this.turno != 0) {
       this.GlobalF.cargando3Seg();
-      var array = [{ "anio": this.anio, 'cuatrimestre': this.cuatrimestre, 'idturno': this.turno,"idusuario":this.idusuario,"idtipo":this.tipo }];
-      
+      var array = [{ "anio": this.anio, 'cuatrimestre': this.cuatrimestre, 'idturno': this.turno, "idusuario": this.idusuario, "idtipo": this.tipo }];
+
       this.ApiProvider.abmGralPost(array, '/materias/comboMaterias').then(Response => {
         this.selectMaterias = Response;
       }).catch(error => {
@@ -119,7 +119,7 @@ export class EstadisticasPage {
     }
   }
   buscar() {
-    if (this.anio != 0 && this.cuatrimestre != 0 && this.turno != 0 && this.idmateria!=0) {
+    if (this.anio != 0 && this.cuatrimestre != 0 && this.turno != 0 && this.idmateria != 0) {
       var array = [{ "anio": this.anio, 'cuatrimestre': this.cuatrimestre, 'idturno': this.turno, 'idmateria': this.idmateria, 'idtipo': this.tipo, 'idusuario': this.idusuario }];
       this.GlobalF.cargando3Seg();
       this.ApiProvider.abmGralPost(array, '/estadisticas/traerAsistenciasCuatrimestrales').then(Response => {
@@ -158,9 +158,7 @@ export class EstadisticasPage {
         }
 
         if (vuelta == this.listado.length) {
-          this.DPresente = DPresente;
-          console.info(DPresente)
-          console.info(this.DPresente)
+          this.DPresente = DPresente;          
           this.Lnomap1 = Lnomap1;
           this.DAusente = DAusente;
           this.Lnomap2 = Lnomap2;
@@ -174,163 +172,154 @@ export class EstadisticasPage {
       }).catch(error => {
         this.GlobalF.error(0);
       })
-    }else{
+    } else {
       this.GlobalF.error('Seleccione los filtros para una busqueda');
     }
   }
   ionViewDidLoad() { }
   graficos() {
-   /* console.info(this.reporte)
-    switch (this.reporte) {
-      case 1:*/
-      
-        this.barChart = new Chart(this.barCanvas.nativeElement, {
+     
+     
+    var dynamicColors = function () {
+      var r = Math.floor(Math.random() * 255);
+      var g = Math.floor(Math.random() * 255);
+      var b = Math.floor(Math.random() * 255);
+      return "rgb(" + r + "," + g + "," + b + ",0.2 )";
+    };
 
-          type: 'bar',
-          data: {
-            labels: this.Lnomap1,
-            datasets: [{
-              label: 'Presencias',
-              data: this.DPresente,
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
-              }]
+
+    var array = [];
+    var i;
+    for (i = 0; i <= this.DPresente.length; i++) {
+      array.push(dynamicColors());
+    }
+
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+      type: 'bar',
+      data: {
+        labels: this.Lnomap1,
+        datasets: [{
+          label: 'Presencias',
+          data: this.DPresente,
+          backgroundColor: array,
+          borderColor: array,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
             }
+          }]
+        }
+      }
+
+    });
+    /* break;
+   case 2:*/
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: this.Lnomap2,
+        datasets: [{
+          label: 'Ausencias',
+          data: this.DAusente,
+          backgroundColor: array,
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+      }
+
+    });
+    /*  break;
+    case 3:*/
+
+    this.pieChart = new Chart(this.pieCanvas.nativeElement, {
+
+      type: 'pie',
+      data: {
+        labels: this.Lnomap3,
+        datasets: [
+          {
+            label: "Justificadas",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.DJustificado,
+            spanGaps: false,
           }
+        ]
+      }
 
-        });
-       /* break;
-      case 2:*/
+    });
+    /*  break;
+    case 4:*/
 
-        this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+    this.polarChart = new Chart(this.polarCanvas.nativeElement, {
 
-          type: 'doughnut',
-          data: {
-            labels: this.Lnomap2,
-            datasets: [{
-              label: 'Ausencias',
-              data: this.DAusente,
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-              ],
-              hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56",
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-              ]
-            }]
+      type: 'polarArea',
+      data: {
+        labels: this.Lnomap4,
+        datasets: [
+          {
+            label: "Medias Faltas",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.DMediaFalta,
+            spanGaps: false,
           }
+        ]
+      }
 
-        });
-      /*  break;
-      case 3:*/
-
-        this.pieChart = new Chart(this.pieCanvas.nativeElement, {
-
-          type: 'pie',
-          data: {
-            labels: this.Lnomap3,
-            datasets: [
-              {
-                label: "Justificadas",
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: this.DJustificado,
-                spanGaps: false,
-              }
-            ]
-          }
-
-        });
-      /*  break;
-      case 4:*/
-
-        this.polarChart = new Chart(this.polarCanvas.nativeElement, {
-
-          type: 'polarArea',
-          data: {
-            labels: this.Lnomap4,
-            datasets: [
-              {
-                label: "Medias Faltas",
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: this.DMediaFalta,
-                spanGaps: false,
-              }
-            ]
-          }
-
-        });
-       /* break;
-      default:
-        this.GlobalF.error('Seleccione un Tipo de Asistencia');
-        break;
-    }*/
+    });
+    /* break;
+   default:
+     this.GlobalF.error('Seleccione un Tipo de Asistencia');
+     break;
+ }*/
   }
   back() {
-    this.GlobalF.cargando();    
+    this.GlobalF.cargando();
     this.navCtrl.setRoot(MenuPage)
   }
   goTo(x) {
